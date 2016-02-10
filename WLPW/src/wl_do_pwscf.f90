b291c7f95534a63b56b1f2d94436ef76dec4878b
Yuk, Simuck F <yuksf@ornl.gov>
2016-02-10 11:18:36 -0500

    Subroutine that performs subsequent PWscf calculations

--- WLPW/src/wl_do_pwscf.f90
+++ WLPW/src/wl_do_pwscf.f90
@@ -1,5 +1,4 @@
 !
-! Copyright (C) 2013 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,

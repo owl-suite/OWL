92e86265eca5e342e7f7370b426053480052dd1c
Yuk, Simuck F <yuksf@ornl.gov>
2016-02-10 11:10:43 -0500

    Main C++ Program of Wang-Landau QE Interface

--- WLPW/src/WL_DFT_Interface.cpp
+++ WLPW/src/WL_DFT_Interface.cpp
@@ -9,9 +9,10 @@
 //-----------------------------------------------------------------
 //
 // Main C++ program calling the Quantum Espresso subroutines for
-// performing PWscf calculations and passing the energy, position,  
+// performing PWscf calculations and passing the energy, position,
 // and cell vector arrays to the Wang-Landau algorithm.
 //
+# include <mpi.h>
 # include <iostream>
 # include <fstream>
 # include "Matrix.hpp"   // Matrix class header
@@ -21,7 +22,7 @@ using namespace std;
 // Define the external Quantum Espresso F90 subroutines
 
 extern "C" {
-  void wl_qe_startup_( );
+  void wl_qe_startup_(int *my_comm);
   void run_pwscf_(int *exit_status);
   void wl_qe_stop_(int *exit_status);
   void get_natom_ener_(int *natom, double *f_etot);
@@ -33,13 +34,17 @@ extern "C" {
 
 // Call the Quantum Espresso subroutines to run PWscf calculations.
 
-int main () {
+int main (int argc, char **argv) {
   int natom;        // A total number of atoms in system
   double f_etot;    // Total energy of system (in eV/unit cell)
   int exit_status;  // Environmental parameter
+  int comm_help;    // Communicator
 
-  wl_qe_startup_( );
-  run_pwscf_(&exit_status);
+  MPI_Init(&argc, &argv);            // Set up the communicator
+  comm_help = MPI_Comm_c2f(MPI_COMM_WORLD);
+
+  wl_qe_startup_(&comm_help);        // Set up the PWscf calculation
+  run_pwscf_(&exit_status);          // Execute the PWscf calculation
   get_natom_ener_(&natom, &f_etot);  // Extract the number of atoms and energy
 
   FILE *energy_file;
@@ -58,7 +63,7 @@ int main () {
   wl_stop_run_(&exit_status);  // Clean up the PWscf run
   wl_pass_array_(&pos_array(0,0));  // Pass the position array to QE
 
-  wl_do_pwscf_(&exit_status);  // Run the subsequent PWscf run
+  wl_do_pwscf_(&exit_status);  // Run the subsequent PWscf calculation
 
   get_natom_ener_(&natom, &f_etot);
 
@@ -69,5 +74,85 @@ int main () {
 
   get_array_(&pos_array(0,0), &cell_array(0,0));
 
-  wl_qe_stop_(&exit_status);  // Finish the PWscf calculations
+  wl_qe_stop_(&exit_status);  // Finish the PWscf calculation
+}
+syuk@cori09:~/3_Programs/1_espresso_5_3_0/WLPW/src> clear
+syuk@cori09:~/3_Programs/1_espresso_5_3_0/WLPW/src> more WL_DFT_Interface.cpp
+//
+// This file is distributed under the terms of the
+// GNU General Public License. See the file `License'
+// in the root directory of the present distribution,
+// or http://www.gnu.org/copyleft/gpl.txt.
+//
+//-----------------------------------------------------------------
+//   WL+DFT Interface Program
+//-----------------------------------------------------------------
+//
+// Main C++ program calling the Quantum Espresso subroutines for
+// performing PWscf calculations and passing the energy, position,
+// and cell vector arrays to the Wang-Landau algorithm.
+//
+# include <mpi.h>
+# include <iostream>
+# include <fstream>
+# include "Matrix.hpp"   // Matrix class header
+
+using namespace std;
+
+// Define the external Quantum Espresso F90 subroutines
+
+extern "C" {
+  void wl_qe_startup_(int *my_comm);
+  void run_pwscf_(int *exit_status);
+  void wl_qe_stop_(int *exit_status);
+  void get_natom_ener_(int *natom, double *f_etot);
+  void get_array_(double *pos_array, double *cell_array);
+  void wl_pass_array_(double *pos_array);
+  void wl_do_pwscf_(int *exit_status);
+  void wl_stop_run_(int *exit_status);
+}
+
+// Call the Quantum Espresso subroutines to run PWscf calculations.
+
+int main (int argc, char **argv) {
+  int natom;        // A total number of atoms in system
+  double f_etot;    // Total energy of system (in eV/unit cell)
+  int exit_status;  // Environmental parameter
+  int comm_help;    // Communicator
+
+  MPI_Init(&argc, &argv);            // Set up the communicator
+  comm_help = MPI_Comm_c2f(MPI_COMM_WORLD);
+
+  wl_qe_startup_(&comm_help);        // Set up the PWscf calculation
+  run_pwscf_(&exit_status);          // Execute the PWscf calculation
+  get_natom_ener_(&natom, &f_etot);  // Extract the number of atoms and energy
+
+  FILE *energy_file;
+  energy_file = fopen("energy.txt", "w");  // Write out the energy
+  fprintf(energy_file, "%f", f_etot);
+  fclose(energy_file);
+
+  Matrix<double> pos_array;  // Set up the position array (in angstrom)
+  pos_array.resize(3,natom);
+
+  Matrix<double> cell_array;  // Set up the cell vector array (in angstrom)
+  cell_array.resize(3,3);
+
+  get_array_(&pos_array(0,0), &cell_array(0,0));
+
+  wl_stop_run_(&exit_status);  // Clean up the PWscf run
+  wl_pass_array_(&pos_array(0,0));  // Pass the position array to QE
+
+  wl_do_pwscf_(&exit_status);  // Run the subsequent PWscf calculation
+
+  get_natom_ener_(&natom, &f_etot);
+
+  FILE *energy_file_2;
+  energy_file_2 = fopen("energy_2.txt", "w");
+  fprintf(energy_file_2, "%f", f_etot);
+  fclose(energy_file_2);
+
+  get_array_(&pos_array(0,0), &cell_array(0,0));
+
+  wl_qe_stop_(&exit_status);  // Finish the PWscf calculation
 }

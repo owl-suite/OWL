f8b48af19c5bc16b96f60f9e209e8c45a2021b47
Yuk, Simuck F <yuksf@ornl.gov>
2016-04-12 10:31:51 -0400

    Update the header file for WL-DFT Interface.

--- WLPW/src/WL_DFT_Interface.hpp
+++ WLPW/src/WL_DFT_Interface.hpp
@@ -5,8 +5,10 @@ extern "C" {
   void run_pwscf_(int *exit_status);
   void wl_qe_stop_(int *exit_status);
   void get_natom_ener_(int *natom, double *f_etot);
-  void get_array_(double *pos_array, double *cell_array);
-  void wl_pass_array_(double *pos_array);
+  void get_pos_array_(double *pos_array);
+  void get_cell_array_(double *cell_array);
+  void pass_pos_array_(double *pos_array);
+  void pass_cell_array_(double *cell_array);
   void wl_do_pwscf_(int *exit_status);
   void wl_stop_run_(int *exit_status);
 }

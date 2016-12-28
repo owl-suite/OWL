81ceb2fcb45fe9b6007192e38bc8bec68102c127
Yuk, Simuck F <yuksf@ornl.gov>
2016-12-28 17:07:28 -0500

    Upload new file

---
+++ OWL_espresso6.0/OWL/src/MCAlgorithms.hpp
@@ -0,0 +1,75 @@
+#ifndef MC_ALGORITHMS_HPP
+#define MC_ALGORITHMS_HPP
+
+#include <mpi.h>
+#include "PhysicalSystemBase.hpp"
+#include "Histogram.hpp"
+//# include "Communications.hpp"
+
+
+// Base class for all Monte Carlo algorithms
+class MonteCarloAlgorithm {
+
+public :
+
+  // Constructor
+  MonteCarloAlgorithm();
+
+  // Destructor
+  virtual ~MonteCarloAlgorithm() {}
+
+  virtual void run(PhysicalSystem*) = 0;
+
+  // Should they be set directly by I/O? (Now set through constructor)
+  int restartFlag;     
+  //PhysicalSystem* physical_system;
+
+protected :
+
+  double currentTime;
+  double lastBackUpTime;
+
+  bool acceptMove {false};
+
+  // MC statistics:
+  unsigned long int totalMCsteps;
+  unsigned long int acceptedMoves;
+  unsigned long int rejectedMoves;
+
+private :
+
+
+};
+
+
+
+class WangLandauSampling : public MonteCarloAlgorithm {
+
+public :
+  
+  WangLandauSampling(int, const char*);
+  ~WangLandauSampling();
+
+  void run(PhysicalSystem*);
+
+private :
+
+  Histogram h;
+
+};
+
+
+// To be implemented.
+class Metropolis : public MonteCarloAlgorithm {
+
+public :
+
+  Metropolis(int, const char*) {}
+  ~Metropolis() {}
+
+  void run(PhysicalSystem*) {}
+
+};
+
+
+#endif

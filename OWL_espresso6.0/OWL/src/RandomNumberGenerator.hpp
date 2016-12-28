81b294cd37e89f50d9277ca659f648104fbd6ed3
Yuk, Simuck F <yuksf@ornl.gov>
2016-12-28 17:09:15 -0500

    Upload new file

---
+++ OWL_espresso6.0/OWL/src/RandomNumberGenerator.hpp
@@ -0,0 +1,26 @@
+#ifndef RANDOM_NUMBER_GENERATOR_HPP
+#define RANDOM_NUMBER_GENERATOR_HPP
+
+#include <random>
+
+// An RNG for global use
+extern std::mt19937 rng;
+//extern int RngSeed;
+
+void initializeRandomNumberGenerator(int = -1);
+
+
+// Returns a random number between [-0.5,0.5]
+inline double getRandomNumber()
+{
+  return (double(rng()) / double(rng.max()) - 0.5);
+}
+
+// Returns a random number between [0,1]
+inline double getRandomNumber2()
+{
+  return (double(rng()) / double(rng.max()));
+}
+
+#endif
+

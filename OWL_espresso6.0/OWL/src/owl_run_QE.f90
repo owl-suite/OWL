89be24d4a7eb26f9745bbc764ee249a573dcebb7
Yuk, Simuck F <yuksf@ornl.gov>
2016-12-28 17:08:25 -0500

    Upload new file

---
+++ OWL_espresso6.0/OWL/src/owl_run_QE.f90
@@ -0,0 +1,84 @@
+!
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!----------------------------------------------------------------------------
+SUBROUTINE owl_qe_startup (my_comm)
+  !----------------------------------------------------------------------------
+  !
+  ! ... Set up the PWscf calculation for Quantum Espresso
+  !
+  USE environment,       ONLY : environment_start
+  USE mp_global,         ONLY : mp_startup
+  USE read_input,        ONLY : read_input_file
+  USE command_line_options, ONLY: input_file_
+  !
+  IMPLICIT NONE
+  INTEGER :: my_comm
+  !
+  CALL mp_startup (my_comm, diag_in_band_group = .true.)
+  CALL environment_start ('PWSCF')
+  !
+  CALL read_input_file ('PW', input_file_ )
+  !
+END SUBROUTINE owl_qe_startup
+  !
+!----------------------------------------------------------------------------
+SUBROUTINE owl_qe_stop (exit_status)
+  !----------------------------------------------------------------------------
+  !
+  ! ... Finish the PWscf calculation for Quantum Espresso
+  !
+  IMPLICIT NONE
+  INTEGER :: exit_status
+  !
+  CALL stop_run( exit_status )
+  CALL do_stop( exit_status )
+  !
+END SUBROUTINE owl_qe_stop
+  !
+!----------------------------------------------------------------------------
+SUBROUTINE owl_stop_run( exit_status )
+  !----------------------------------------------------------------------------
+  !
+  ! ... Modified subroutine to restart the PWscf calculation after receiving
+  ! ... the Wang-Landau inputs
+  !
+  USE io_global,          ONLY : ionode
+  USE mp_global,          ONLY : mp_global_end
+  USE environment,        ONLY : environment_end
+  USE io_files,           ONLY : iuntmp, seqopn
+  !
+  IMPLICIT NONE
+  !
+  INTEGER, INTENT(IN) :: exit_status
+  LOGICAL             :: exst, opnd, lflag
+  !
+  lflag = ( exit_status == 0 )
+  IF ( lflag ) THEN
+     !
+     ! ... remove files needed only to restart
+     !
+     CALL seqopn( iuntmp, 'restart', 'UNFORMATTED', exst )
+     CLOSE( UNIT = iuntmp, STATUS = 'DELETE' )
+     !
+     IF ( ionode ) THEN
+        CALL seqopn( iuntmp, 'update', 'FORMATTED', exst )
+        CLOSE( UNIT = iuntmp, STATUS = 'DELETE' )
+        CALL seqopn( iuntmp, 'para', 'FORMATTED', exst )
+        CLOSE( UNIT = iuntmp, STATUS = 'DELETE' )
+     END IF
+     !
+  END IF
+  !
+  CALL close_files(lflag)
+  !
+  CALL print_clock_pw()
+  !
+  CALL clean_pw( .FALSE. )   ! ... Need to restart the subsequent PWscf runs
+  !
+END SUBROUTINE owl_stop_run
+  !
+!----------------------------------------------------------------------------

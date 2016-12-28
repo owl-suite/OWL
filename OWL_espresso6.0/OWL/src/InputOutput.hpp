492eea76ebc1e71f7b9749254bcdfa2becbda6a3
Yuk, Simuck F <yuksf@ornl.gov>
2016-12-28 17:06:44 -0500

    Upload new file

---
+++ OWL_espresso6.0/OWL/src/InputOutput.hpp
@@ -0,0 +1,9 @@
+#ifndef INPUT_OUTPUT_HPP
+#define INPUT_OUTPUT_HPP
+
+#include <iostream>
+#include "Globals.hpp"
+
+void readMainInputFile(const char* inputFileName, SimulationInfo& sim_info);
+
+#endif

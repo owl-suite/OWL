594322048975bbc3a211373e6e7b96e489d3b35d
Ying Wai Li <yingwaili@lanl.gov>
2020-08-07 23:59:47 -0600

    Implementation of customized interactions in 3D Heisenberg model. Some renaming.

--- src/PhysicalSystems/Ising2D.cpp
+++ src/PhysicalSystems/Ising2D.cpp
@@ -155,7 +155,6 @@ void Ising2D::getObservables()
     observables[1] += spin[CurX*Size+CurY] - CurType;
     observables[2]  = abs(observables[1]);
     //printf("observables = %10.5f %10.5f %10.5f\n", observables[0], observables[1], observables[2]);
-
   }
 
 }

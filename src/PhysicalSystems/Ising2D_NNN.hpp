197a62c67da0d357dbd08e94eabae0be12d56c55
Ying Wai Li <yingwaili@lanl.gov>
2022-09-21 18:35:03 -0600

    Implementation for 2D Ising model with nearest neighbor interactions.

---
+++ src/PhysicalSystems/Ising2D_NNN.hpp
@@ -0,0 +1,46 @@
+#ifndef ISING2D_NNN_HPP
+#define ISING2D_NNN_HPP
+
+#include <filesystem>
+#include "PhysicalSystemBase.hpp"
+
+class Ising2D_NNN : public PhysicalSystem {
+
+public :
+
+  Ising2D_NNN(const char* spinConfigFile = "config_initial.dat"); 
+  ~Ising2D_NNN();
+
+  //void readCommandLineOptions()                         override;
+  void writeConfiguration(int = 0, const char* = NULL)  override;
+  void getObservables()                                 override;
+  void doMCMove()                                       override;
+  void acceptMCMove()                                   override;
+  void rejectMCMove()                                   override;
+
+  void buildMPIConfigurationType();
+
+private :
+
+  unsigned int                numExchangeInteractions {2};
+  std::vector<ObservableType> exchangeInteraction;
+
+  typedef int SpinDirection;
+
+  unsigned int Size;
+
+  // Old configuration
+  unsigned int CurX, CurY;
+  SpinDirection CurType;
+
+  // New configuration
+  SpinDirection* spin;             // make it a flat array for MPI to operate on
+
+  // Initialization:
+  void   readSpinConfigFile(const std::filesystem::path& spinConfigFile);
+  void   readHamiltonian(const std::filesystem::path& mainInputFile);
+  void   initializeSpinConfiguration(unsigned int initial = 0);
+
+};
+
+#endif
